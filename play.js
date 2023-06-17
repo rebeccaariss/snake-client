5d4470dcda97a8e50d85d016a5ab2f53b85e48cc
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-06-17 00:52:09 -0400

    Add code to end game with CTRL + C

--- play.js
+++ play.js
@@ -1,3 +1,25 @@
 const { connect } = require("./client.js");
 
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", (key) => {
+    handleUserInput(key);
+  });
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+
+setupInput();
+handleUserInput();
