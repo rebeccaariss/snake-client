f7246633470b8a7fa3a155f7821d41f171c89274
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-06-16 20:42:05 -0400

    Print data/message received from server

--- play.js
+++ play.js
@@ -1,14 +1,26 @@
+// CLIENT
+// CLIENT
+// CLIENT
+// CLIENT
+// CLIENT
+// CLIENT
+// CLIENT
+
 const net = require("net");
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: // IP address here,
-    port: // PORT number here,
+    host: "localhost",
+    port: 50541,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
 
   return conn;
 };
