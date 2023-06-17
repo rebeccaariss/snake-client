96d6138128c399064627f5abcd4285063720757f
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-06-16 21:45:00 -0400

    Implement ES6 shorthand syntax across existing modules

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  console.log("Connecting ...");
+  // Note that object assigned to variable "conn" is an
+  // instance of the Socket class in Node's net library.
+  // See Socket methods in node docs:
+  // https://nodejs.org/api/net.html#class-netsocket
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  }, () => console.log("Connected to ssssserver 🐍"));
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,33 +1,3 @@
-// CLIENT
-// CLIENT
-// CLIENT
-// CLIENT
-// CLIENT
-// CLIENT
-// CLIENT
+const { connect } = require("./client.js");
 
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  // Note that object assigned to variable "conn" is an
-  // instance of the Socket class in Node's net library.
-  // See Socket methods in node docs:
-  // https://nodejs.org/api/net.html#class-netsocket
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  }, () => console.log("Connected to ssssserver 🐍"));
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
-
-console.log("Connecting ...");
 connect();
